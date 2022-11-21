faf6e767e335879f0fe533e04bfd1d3606e1ae1a
sonjaolkkonen <sonja.olkkonen@gmail.com>
2022-11-21 04:22:17 +0200

    add test

---
+++ src/tests/sudoku_test.py
@@ -0,0 +1,12 @@
+import pygame
+import requests
+import unittest
+from sudoku import Sudoku
+
+class TestSudoku(unittest.TestCase):
+    def setUp(self):
+        self.sudoku = Sudoku()
+
+    def test_grid_has_nine_3x3_grids(self):
+        self.sudoku.new_game()
+        self.assertEqual(len(self.grid), 9)
