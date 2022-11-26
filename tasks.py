f3214202fea58d7dc172ef5cc5fad5ce1f7afa81
sonjaolkkonen <sonja.olkkonen@gmail.com>
2022-11-26 04:51:36 +0200

    added pylint task

--- tasks.py
+++ tasks.py
@@ -5,6 +5,11 @@ def start(ctx):
     ctx.run("python3 src/sudoku.py", pty=True)
 
 @task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
+
+
+@task
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
