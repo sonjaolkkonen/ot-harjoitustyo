bdc83132bfe21b00e874fec0c6ae80db9d379123
sonjaolkkonen <sonja.olkkonen@gmail.com>
2022-12-25 14:22:09 +0200

    edited test

--- src/tests/services/sudoku_test.py
+++ src/tests/services/sudoku_test.py
@@ -3,16 +3,3 @@ import pygame
 from services.game_loop import GameLoop
 
 
-class TestSudoku(unittest.TestCase):
-    def setUp(self):
-        position = None
-        screen = pygame.display.set_mode((550, 550))
-        font = pygame.font.SysFont('Arial', 30)
-        number = None
-        grid = None
-
-        self.game_loop = GameLoop(position, screen, font, number, grid, level="easy")
-
-    def test_grid_has_nine_3x3_grids(self):
-        self.game_loop.new_game()
-        self.assertEqual(len(self.grid), 9)
