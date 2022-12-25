f0f14fa1cf0adeddf6a95f313a7227c6ea26f7ee
sonjaolkkonen <sonja.olkkonen@gmail.com>
2022-12-25 14:08:20 +0200

    edited test

--- src/tests/repositories/score_repository_test.py
+++ src/tests/repositories/score_repository_test.py
@@ -2,20 +2,17 @@ import unittest
 import os
 from repositories.score_repository import ScoreRepository
 
+dirname = os.path.dirname(__file__)
+scores_file = os.path.join(dirname, "../..", "score.txt")
+
 class TestScoreRepository(unittest.TestCase):
     def setUp(self):
         self.score = 10
-        dirname = os.path.dirname(__file__)
-        self.scores_file = os.path.join(dirname, "../..", "score.txt")
 
     def test_all_scores(self):
 
-        with open(self.scores_file, "r") as scores:
-            content = scores.read().splitlines()
-            if content == []:
-                content == []
-            else:
-                content == []
+        with open(scores_file, "r") as scores:
+            scores.truncate()
 
         ScoreRepository.add_new_score(self, self.score)
         scores = ScoreRepository.all_scores(self)
